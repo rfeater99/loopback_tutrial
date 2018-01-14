cdc41f504c8505d0671095365b0d21dcbae93645
rfeather99 <rfeather99@yahoo.co.jp>
2018-01-14 23:34:42 +0900

    add project.

---
+++ server/datasources.production.js
@@ -0,0 +1,10 @@
+var env = JSON.parse(process.env.VCAP_SERVICES);
+
+console.log("load datasources.production.js")
+module.exports = {
+  db:{
+    connector:'cloudant',
+    url:env['cloudantNoSQLDB'][0].credentials.url,
+    database:'memo'
+  }
+}
